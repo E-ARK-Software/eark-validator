1583cf00e3a32d0f877e6d4c2140217281c99a22
Jakub Janaszewski (DOC) <DOC@kmd.dk>
2024-08-07 19:53:10 +0200

    Fixed CSIP26 validation

--- eark_validator/ipxml/schematron.py
+++ eark_validator/ipxml/schematron.py
@@ -34,6 +34,7 @@ from lxml.isoschematron import Schematron
 
 from eark_validator.const import NO_PATH, NOT_FILE
 from .resources import schematron as SCHEMATRON
+from .resources import vocabs as vocabularies
 
 SCHEMATRON_NS = '{http://purl.oclc.org/dsdl/schematron}'
 SVRL_NS = '{http://purl.oclc.org/dsdl/svrl}'
@@ -41,14 +42,18 @@ SVRL_NS = '{http://purl.oclc.org/dsdl/svrl}'
 class SchematronTests():
     __vocabulary_definitions = {
         '@TYPE': 'https://earkcsip.dilcis.eu/schema/CSIPVocabularyContentCategory.xml',
-                '@csip:CONTENTINFORMATIONTYPE': 'https://earkcsip.dilcis.eu/schema/CSIPVocabularyContentInformationType.xml',
-                '@csip:OAISPACKAGETYPE': 'https://earkcsip.dilcis.eu/schema/CSIPVocabularyOAISPackageType.xml'}
+        '@csip:CONTENTINFORMATIONTYPE': 'https://earkcsip.dilcis.eu/schema/CSIPVocabularyContentInformationType.xml',
+        '@csip:OAISPACKAGETYPE': 'https://earkcsip.dilcis.eu/schema/CSIPVocabularyOAISPackageType.xml',
+        '@STATUS': 'https://earkcsip.dilcis.eu/schema/CSIPVocabularyStatus.xml'
+    }
 
     tests = {}
 
     def __init__(self):
         for attribute, vocabulary_uri in self.__vocabulary_definitions.items():
             self.tests[attribute + '_vocabulary_test'] = self.__create_vocabulary_test(attribute, vocabulary_uri)
+        
+        self.tests['@MIMETYPE_IANA_test'] = self.___create_IANA_test()
 
     def __create_vocabulary_test(self, attribute: str, vocabulary_uri: str) -> str:
         vocabulary_tests = []
@@ -64,6 +69,15 @@ class SchematronTests():
             vocabulary_tests.append(f"({attribute} = '{vocabulary_item}')")
 
         return ' or '.join(vocabulary_tests)
+    
+    def ___create_IANA_test(self) -> str:
+        mime_tests = []
+        with open(str(files(vocabularies).joinpath('IANA.txt')), 'r') as iana:
+            for mime_type in iana:
+                mime_type = mime_type.rstrip("\n")
+                mime_tests.append(f"(@MIMETYPE = '{mime_type}')")
+
+        return ' or '.join(mime_tests)
 
 schematron_tests = SchematronTests()
 
